3e769f102853aa6967b4286e0f47a1c0bea1094d
TchaPa <TchaPa@DESKTOP-RJ8U3LT>
2024-08-24 12:49:09 -0300

    Adicionar arquivos de projeto.

---
+++ IMCFinal/Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace IMCFinal
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.lblNome = new System.Windows.Forms.Label();
+            this.lblPeso = new System.Windows.Forms.Label();
+            this.lblAltura = new System.Windows.Forms.Label();
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.txtPeso = new System.Windows.Forms.TextBox();
+            this.txtAltura = new System.Windows.Forms.TextBox();
+            this.btnCalcularIMC = new System.Windows.Forms.Button();
+            this.lblResultado = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblNome
+            // 
+            this.lblNome.AutoSize = true;
+            this.lblNome.Location = new System.Drawing.Point(12, 15);
+            this.lblNome.Name = "lblNome";
+            this.lblNome.Size = new System.Drawing.Size(35, 13);
+            this.lblNome.TabIndex = 0;
+            this.lblNome.Text = "Nome";
+            // 
+            // lblAltura
+            // 
+            this.lblAltura.AutoSize = true;
+            this.lblAltura.Location = new System.Drawing.Point(12, 67);
+            this.lblAltura.Name = "lblAltura";
+            this.lblAltura.Size = new System.Drawing.Size(58, 13);
+            this.lblAltura.TabIndex = 2;
+            this.lblAltura.Text = "Altura (m)";
+            // 
+            // lblPeso
+            // 
+            this.lblPeso.AutoSize = true;
+            this.lblPeso.Location = new System.Drawing.Point(12, 41);
+            this.lblPeso.Name = "lblPeso";
+            this.lblPeso.Size = new System.Drawing.Size(64, 13);
+            this.lblPeso.TabIndex = 1;
+            this.lblPeso.Text = "Peso (kg)";
+            // 
+            // txtNome
+            // 
+            this.txtNome.Location = new System.Drawing.Point(82, 12);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(100, 20);
+            this.txtNome.TabIndex = 3;
+            // txtAltura
+            // 
+            this.txtAltura.Location = new System.Drawing.Point(82, 64);
+            this.txtAltura.Name = "txtAltura";
+            this.txtAltura.Size = new System.Drawing.Size(100, 20);
+            this.txtAltura.TabIndex = 5;
+            // 
+            // txtPeso
+            // 
+            this.txtPeso.Location = new System.Drawing.Point(82, 38);
+            this.txtPeso.Name = "txtPeso";
+            this.txtPeso.Size = new System.Drawing.Size(100, 20);
+            this.txtPeso.TabIndex = 4;
+            // 
+            // 
+            // btnCalcularIMC
+            // 
+            this.btnCalcularIMC.Location = new System.Drawing.Point(82, 90);
+            this.btnCalcularIMC.Name = "btnCalcularIMC";
+            this.btnCalcularIMC.Size = new System.Drawing.Size(100, 23);
+            this.btnCalcularIMC.TabIndex = 6;
+            this.btnCalcularIMC.Text = "Calcular IMC";
+            this.btnCalcularIMC.UseVisualStyleBackColor = true;
+            this.btnCalcularIMC.Click += new System.EventHandler(this.btnCalcularIMC_Click);
+            // 
+            // lblResultado
+            // 
+            this.lblResultado.AutoSize = true;
+            this.lblResultado.Location = new System.Drawing.Point(12, 126);
+            this.lblResultado.Name = "lblResultado";
+            this.lblResultado.Size = new System.Drawing.Size(0, 13);
+            this.lblResultado.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(284, 161);
+            this.Controls.Add(this.lblResultado);
+            this.Controls.Add(this.btnCalcularIMC);
+            this.Controls.Add(this.txtAltura);
+            this.Controls.Add(this.txtPeso);
+            this.Controls.Add(this.txtNome);
+            this.Controls.Add(this.lblAltura);
+            this.Controls.Add(this.lblPeso);
+            this.Controls.Add(this.lblNome);
+            this.Name = "Form1";
+            this.Text = "Calculadora de IMC";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+
+        private System.Windows.Forms.Label lblNome;
+        private System.Windows.Forms.Label lblPeso;
+        private System.Windows.Forms.Label lblAltura;
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.TextBox txtPeso;
+        private System.Windows.Forms.TextBox txtAltura;
+        private System.Windows.Forms.Button btnCalcularIMC;
+        private System.Windows.Forms.Label lblResultado;
+    }
+}
---
+++ IMCFinal/Forms1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+using System.Configuration;
+
+namespace IMCFinal
+{
+    public partial class Form1 : Form
+    {
+        private string connectionString;
+
+        public Form1()
+        {
+            InitializeComponent();
+            connectionString = ConfigurationManager.ConnectionStrings["Teste"].ConnectionString;
+        }
+
+        private void btnCalcularIMC_Click(object sender, EventArgs e)
+        {
+            string nome = txtNome.Text;
+            if (double.TryParse(txtPeso.Text, out double peso) && double.TryParse(txtAltura.Text, out double altura))
+            {
+                double imc = peso / (altura * altura);
+                lblResultado.Text = $"{nome}, seu IMC é {imc:F2}";
+
+                SaveRecord(nome, altura, peso, imc);
+            }
+            else
+            {
+                lblResultado.Text = "Por favor, insira valores válidos para peso e altura.";
+            }
+        }
+
+        private void SaveRecord(string nome, double altura, double peso, double imc)
+        {
+            using (var connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                string insertQuery = "INSERT INTO IMC (Nome_User, Altura, Peso, IMC) VALUES (@Nome_User,@Altura, @Peso, @IMC)";
+
+                using (var command = new SqlCommand(insertQuery, connection))
+                {
+                    command.Parameters.AddWithValue("@Nome_User", nome);
+                    command.Parameters.AddWithValue("@Altura", altura);
+                    command.Parameters.AddWithValue("@Peso", peso);
+                    command.Parameters.AddWithValue("@IMC", imc);
+
+                    command.ExecuteNonQuery();
+                }
+            }
+        }
+    }
+}
